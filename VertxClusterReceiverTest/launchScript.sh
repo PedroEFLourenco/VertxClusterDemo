f5aa4ab33fbd3ac0db1d431657ad88310716ea15
Pedro Lourenço <pedro.lourenco@outlook.pt>
2019-01-27 20:56:25 +0000

    Commit Demo Repo

--- VertxClusterReceiverTest/launchScript.sh
+++
@@ -1,3 +0,0 @@
-
-export POD_IP=`ifconfig eth0 | grep inet | awk '{print $2}' | cut -d/ -f1`
-java -Dvertx.hazelcast.config=cluster.xml -jar vertx-cluster-receiver-1.0-SNAPSHOT-fat.jar -cluster -cluster-host $POD_IP -cluster-port 5702
